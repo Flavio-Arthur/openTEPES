9889508b3ffb8ad31b49922a36f1ea45d5f493bd
Flavio-Arthur <ferreira.flavioarthur@gmail.com>
2023-08-15 05:12:47 -0300

    Sync

---
+++ openTEPES/teste.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 13 20:18:08 2023
+
+@author: flavioalferreira
+"""
+from openTEPES.openTEPES import openTEPES_run
--- tests/test_run.py
+++ tests/test_run.py
@@ -1,4 +1,4 @@
-# import argparse
+
 import os
 # from openTEPES.openTEPES import openTEPES_run
 # import openTEPES.openTEPES as oT
@@ -10,18 +10,9 @@ TEST_PATH = CWD + '/openTEPES'
 os.chdir(TEST_PATH)
 
 CASE = "9n"
-# parser = argparse.ArgumentParser(description='Introducing main parameters.')
-# parser.add_argument('--case', type=str, default=None)
-# parser.add_argument('--dir', type=str, default=None)
-# parser.add_argument('--solver', type=str, default=None)
-# DIR = os.path.dirname(openTEPES.__file__)
-DIR = TEST_PATH
+DIR='/Users/flavioalferreira/opt/anaconda3/lib/python3.9/site-packages/openTEPES'
 SOLVER = "gurobi"
+LOG='No'
+RESULT='No'
 
 oT.openTEPES_run(DIR, CASE, SOLVER)
-# def test_openTEPES():
-#     assert mTEPES == oT.openTEPES_run(DIR, CASE, SOLVER)
-
-
-# if __name__ == "__main__":
-#     test_openTEPES(DIR, CASE, SOLVER)
